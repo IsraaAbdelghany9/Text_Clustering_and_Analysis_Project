4bcd52399362f07ff09ba398c11724f8160e5a0d
IsraaAbdelghany9 <https://github.com/IsraaAbdelghany9>
2025-03-26 16:32:21 +0200

    utils.py

---
+++ ML_2_proj/utils.py
@@ -0,0 +1,76 @@
+import re
+import string
+import matplotlib.pyplot as plt
+from nltk.corpus import stopwords
+from nltk.tokenize import TweetTokenizer
+from nltk.stem.wordnet import WordNetLemmatizer
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+
+
+
+def process_tweet(tweet):
+    """Process tweet function.
+    Input:
+        tweet: a string containing a tweet
+    Output:
+        tweets_clean: a list of words containing the processed tweet
+
+    """
+    lemmatizer = WordNetLemmatizer()
+    stopwords_english = stopwords.words('english')
+    # remove stock market tickers like $GE
+    tweet = re.sub(r'\$\w*', '', tweet)
+    # remove old style retweet text "RT"
+    tweet = re.sub(r'^RT[\s]+', '', tweet)
+    # remove hyperlinks    
+    tweet = re.sub(r'https?://[^\s\n\r]+', '', tweet)
+    # remove hashtags
+    # only removing the hash # sign from the word
+    tweet = re.sub(r'#', '', tweet)
+    # remove numbers
+    tweet = re.sub(r'\d+', '', tweet)
+    # tokenize tweets
+    tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True,
+                               reduce_len=True)
+    tweet_tokens = tokenizer.tokenize(tweet)
+
+    tweets_clean = []
+    for word in tweet_tokens:
+        if (word not in stopwords_english and  # remove stopwords
+                word not in string.punctuation):  # remove punctuation
+            # tweets_clean.append(word)
+            lemma_word = lemmatizer.lemmatize(word)  # lemmatizing word
+            tweets_clean.append(lemma_word)
+
+    return tweets_clean
+
+
+def plot_elbow_and_silhouette(X, k_range=range(2, 10)):
+    inertia = []
+    silhouette_scores = []
+    K_range = k_range  # Checking for k from 2 to 10
+
+    for k in K_range:
+        model = KMeans(n_clusters=k, init='k-means++', max_iter=100, n_init=10, random_state=42)
+        model.fit(X)
+    
+        inertia.append(model.inertia_)  # Sum of squared distances to closest cluster center
+        silhouette_scores.append(silhouette_score(X, model.labels_))  # Measure of cluster quality
+
+    # Plot Elbow Method
+    plt.figure(figsize=(10, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(K_range, inertia, marker='o', linestyle='-')
+    plt.xlabel('Number of Clusters (k)')
+    plt.ylabel('Inertia')
+    plt.title('Elbow Method')
+
+    # Plot Silhouette Scores
+    plt.subplot(1, 2, 2)
+    plt.plot(K_range, silhouette_scores, marker='o', linestyle='-')
+    plt.xlabel('Number of Clusters (k)')
+    plt.ylabel('Silhouette Score')
+    plt.title('Silhouette Analysis')
+
+    plt.show()
