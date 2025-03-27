ba079588e22fef37709d701c72c1d873293e8713
IsraaAbdelghany9 <https://github.com/IsraaAbdelghany9>
2025-03-27 14:10:30 +0200

    edit visualization file

--- ML_2_proj/src/clustering.py
+++ ML_2_proj/src/clustering.py
@@ -1,7 +1,7 @@
 from sklearn.cluster import KMeans
 from scipy.cluster.hierarchy import linkage
 
-def apply_kmeans(X, n_clusters=5):
+def apply_kmeans(X, n_clusters):
     """
 
     Applies K-Means clustering.
@@ -14,7 +14,7 @@ def apply_kmeans(X, n_clusters=5):
         KMeans: Fitted KMeans model.
 
     """
-    model = KMeans(n_clusters=n_clusters, init='k-means++', max_iter=100, n_init=10, random_state=42)
+    model = KMeans(n_clusters=n_clusters, init='k-means++', max_iter=1000, n_init=100, random_state=42)
     model.fit(X)
     return model
 
